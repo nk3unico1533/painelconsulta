bbe444b16e6adb8c6a4c8c620881703bb1f1ca74
nk3unico1533 <nk3unico@outlook.com>
2025-11-17 10:19:31 -0300

    Delete components directory

--- components/Card.jsx
+++
@@ -1,8 +0,0 @@
-export default function Card({ title, children }) {
-  return (
-    <div className="bg-[#0d0d11] p-5 rounded-xl border border-purple-500/30">
-      <h3 className="text-lg font-semibold mb-3">{title}</h3>
-      {children}
-    </div>
-  );
-}
